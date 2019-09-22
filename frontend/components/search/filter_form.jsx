20553f5ce07ed751a8c3f5ec422b422a1fbefd8c
Patrick <patrick.yrkim@gmail.com>
2019-09-21 23:28:20 -0700

    Initializing search filters

---
+++ frontend/components/search/filter_form.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class FilterForm extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    handleUpdate() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
