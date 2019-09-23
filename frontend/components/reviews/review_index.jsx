dd688b2584b18ab97972a64fbb6e34b706d50331
Patrick <patrick.yrkim@gmail.com>
2019-09-22 17:24:50 -0700

    initialized review index

---
+++ frontend/components/reviews/review_index.jsx
@@ -0,0 +1,31 @@
+// Phase 13: Reviews
+// Show reviews of a bench on BenchShow.Reviews for a bench should comprise:
+
+//     A rating from 1 to 5.
+// A comment field.
+// Add a ReviewIndex and ReviewForm.ReviewIndex should show the average score for
+//  a bench and also list the reviews for that bench.Modify and add the 
+//  appropriate API endpoints, actions, reducers, utils, and components.
+ 
+
+ import React from 'react';
+
+ export default class ReviewIndex extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        
+    }
+
+    render() {
+        // const reviews = this.props.reviews
+
+        return (
+            <div>
+                {reviews}
+            </div>
+        )
+    }
+ }
