bd506a40918e46d3db7f133ae9fb43bfee48e29a
Patrick <patrick.yrkim@gmail.com>
2019-09-25 17:51:33 -0700

    Update seeds user and business

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,12 +25,12 @@ class User < ApplicationRecord
     end
 
     def reset_session_token
-        self.session_token = Secure.Random.urlsafe_base64
+        self.session_token = SecureRandom::urlsafe_base64
         self.save!
         self.session_token
     end
 
     def ensure_session_token
-        self.session_token ||= Secure.Random.urlsafe_base64
+        self.session_token ||= SecureRandom::urlsafe_base64
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,11 @@ User.destroy_all
 
 #users
 user_1 = User.create!(first_name: 'Mickey', last_name: 'Mouse', email: 'mickey@email.com')
-# user_2 = User.create!()
+user_2 = User.create!(first_name: 'Donald', last_name: 'Duck', email: 'donald@email.com')
 
 #businesses
 business_1 = Business.create!(name: 'Salt and Straw', address: '2201 Fillmore Street', city: 'San Francisco', state: 'CA', zip_code: 94115, website: 'saltandstraw.com', description: 'Classic SF Flavors', phone_number: '415-829-2803', price_range: '$', credit_card: 'Yes', parking: 'No', wifi: 'No', restroom: 'No', category: 'ice cream', rating: '5', open_time: '11:00 AM', close_time: '11:00PM')
-# business_2 = Business.create!()
+business_2 = Business.create!(name: 'Fentons', address: '4226 Piedmont Ave', city: 'Oakland', state: 'CA', zip_code: 94611, website: 'fentonscreamery.com', description: 'ice cream parlor known for sundaes and variety of flavors, featured in UP', phone_number: '510-658-7000', price_range: '$$', credit_card: 'Yes', parking: 'Yes', wifi: 'No', restroom: 'Yes', category: 'ice cream', rating: '5', open_time: '11:00 AM', close_time: '11:00PM')
 
 # #reviews
 # review_1 = Review.create!()
