2f0a6f26a822e69498da7a4d5def1c611ffaa97e
Patrick <patrick.yrkim@gmail.com>
2019-09-26 19:17:01 -0700

    Initialized review_index_item

---
+++ frontend/components/reviews/review_index_item.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default class ReviewIndexItem extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        let { review, currentUser } = this.props
+
+        return (
+            <div>
+                <div>
+                    
+                </div>
+            </div>
+        )
+    }
+}
