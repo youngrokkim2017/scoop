4ca3054bc0f076f0eb83a280601cf32265c93a28
Patrick <patrick.yrkim@gmail.com>
2019-10-01 14:21:14 -0700

    Fix webpack path

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
     entry: './frontend/scoop.jsx',
     output: {
-        path: path.resolve(__dirname),
+        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
         filename: './bundle.js',
     },
     module: {
