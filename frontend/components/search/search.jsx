22481a5e385e2864bfb7a53afbebe51855df66f7
Patrick <patrick.yrkim@gmail.com>
2019-10-07 23:31:07 -0700

    Update home page styling

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 // import BusinessMap from '../business/business_map';
 // import BusinessIndex from '../business/business_index';
 // import {updateFilter} from '../../actions/filter_actions';
@@ -47,7 +47,7 @@ class Search extends React.Component {
                             type="text" 
                             onChange={this.handleChange('find')} 
                             value={this.state.find} 
-                            placeholder="ice cream, frozen yogurt, gelato..."
+                            placeholder="desserts, ice cream, frozen yogurt, gelato..." //shaved ice, soft serve, ..etc
                             />
                     </label>
                     {/* <div>
@@ -67,7 +67,59 @@ class Search extends React.Component {
                     {/* <input className="search-button" type="submit" onClick={this.handleSubmit}/> */}
                     {/* <Link to="/businesses">Search</Link> */}
                     <button className="search-button" onClick={this.handleSubmit}>Search</button>
+
                 </form>
+
+                <div className="search-shortcuts">
+                    <a onClick={this.handleSubmit}>Creamery</a>
+                    <a onClick={this.handleSubmit}>Frozen Yogurt</a>
+                    <a onClick={this.handleSubmit}>Gelato</a>
+                    <a onClick={this.handleSubmit}>Shaved Ice</a>
+                </div>
+
+                <div className="favorites">
+                    <div className="favorites-header">
+                        <h2>Favorites</h2>
+                    </div>
+
+                    <div className="favorites-list">
+                        <div className="favorites-birite">
+                            <div className="birite-photo">
+
+                            </div>
+                            <div className="birite-info">
+                                <Link to="/businesses/1">
+                                    Bi-Rite Creamery
+                                    {/* {this.props.businesses.name} */}
+                                </Link>
+                            </div>
+                        </div>
+
+                        <div className="favorites-saltstraw">
+                            <div className="saltstraw-photo">
+                            
+                            </div>
+                            <div className="saltstraw-info">
+                                <Link to="/businesses/3">
+                                    Salt & Straw
+                                    {/* {this.props.businesses.name} */}
+                                </Link>
+                            </div>
+                        </div>
+
+                        <div className="favorites-fentons">
+                            <div className="fentons-photo">
+
+                            </div>
+                            <div className="fentons-info">
+                                <Link to="/businesses/4">
+                                    Fentons
+                                    {/* {this.props.businesses.name} */}
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
